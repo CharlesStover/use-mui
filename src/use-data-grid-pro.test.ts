5c5411894903f9f56cd1ec3a0859e322f842195c
Charles Stover <github@charlesstover.com>
2021-10-13 03:01:52 -0500

    useDataGridPro

--- src/use-data-grid-pro.test.ts
+++ src/use-data-grid-pro.test.ts
@@ -1,4 +1,51 @@
 import { useDataGridPro } from '.';
 import describeHook from './test-utils/describe-hook';
 
-describeHook(useDataGridPro, []);
+const DEFAULT_PAGE = 0;
+const DEFAULT_PAGE_SIZE = 10;
+const TEST_ERROR: Error = new Error('test error message');
+const TEST_PAGE = 2;
+const TEST_PAGE_SIZE = 25;
+
+describeHook(useDataGridPro, [
+  {
+    args: [TEST_ERROR],
+    callback: 'onError',
+    defaultGetter: 'defaultError',
+    defaultValue: undefined,
+    getter: 'error',
+    handler: 'handleError',
+    setter: 'setError',
+    value: TEST_ERROR,
+  },
+  {
+    args: ['___'],
+    callback: 'onFilterModelChange',
+    defaultGetter: 'defaultFilterModel',
+    defaultValue: undefined,
+    getter: 'filterModel',
+    handler: 'handleFilterModelChange',
+    setter: 'setFilterModel',
+    value: '___',
+  },
+  {
+    args: [TEST_PAGE],
+    callback: 'onPageChange',
+    defaultGetter: 'defaultPage',
+    defaultValue: DEFAULT_PAGE,
+    getter: 'page',
+    handler: 'handlePageChange',
+    setter: 'setPage',
+    value: TEST_PAGE,
+  },
+  {
+    args: [TEST_PAGE_SIZE],
+    callback: 'onPageSizeChange',
+    defaultGetter: 'defaultPageSize',
+    defaultValue: DEFAULT_PAGE_SIZE,
+    getter: 'pageSize',
+    handler: 'handlePageSizeChange',
+    setter: 'setPageSize',
+    value: TEST_PAGE_SIZE,
+  },
+]);
--- src/use-data-grid-pro.ts
+++ src/use-data-grid-pro.ts
@@ -1,7 +1,107 @@
-export interface State {
-  readonly error: Error;
+import type { Dispatch, SetStateAction } from 'react';
+import { useState } from 'react';
+import DEFAULT_PROPS from './constants/default-props';
+import useHandler from './hooks/use-handler';
+
+interface GridFilterModel {
+  readonly _: unknown;
 }
 
-export default function useDataGridPro(): void {
-  return;
+interface GridSelectionModel {
+  readonly _: unknown;
+}
+
+interface GridSortModel {
+  readonly _: unknown;
+}
+
+interface Props {
+  readonly defaultError?: Error | undefined;
+  readonly defaultFilterModel?: GridFilterModel | undefined;
+  readonly defaultPage?: number | undefined;
+  readonly defaultPageSize?: number | undefined;
+  readonly defaultSelectionModel?: GridSelectionModel | undefined;
+  readonly defaultSortModel?: GridSortModel | undefined;
+  readonly onError?: ((error: Error) => void) | undefined;
+  readonly onFilterModelChange?: ((model: GridFilterModel) => void) | undefined;
+  readonly onPageChange?: ((page: number) => void) | undefined;
+  readonly onPageSizeChange?: ((pageSize: number) => void) | undefined;
+  readonly onSortModelChange?: ((model: GridSortModel) => void) | undefined;
+  readonly onSelectModelChange?:
+    | ((model: GridSelectionModel) => void)
+    | undefined;
+}
+
+export interface State {
+  readonly error: Error | undefined;
+  readonly filterModel: GridFilterModel | undefined;
+  readonly handleError: (error: Error) => void;
+  readonly handleFilterModelChange: (modal: GridFilterModel) => void;
+  readonly handlePageChange: (page: number) => void;
+  readonly handlePageSizeChange: (pageSize: number) => void;
+  readonly handleSelectionModelChange: (model: GridSelectionModel) => void;
+  readonly handleSortModelChange: (model: GridSortModel) => void;
+  readonly page: number;
+  readonly pageSize: number;
+  readonly selectionModel: GridSelectionModel | undefined;
+  readonly setError: Dispatch<SetStateAction<Error | undefined>>;
+  readonly setPage: Dispatch<SetStateAction<number>>;
+  readonly setPageSize: Dispatch<SetStateAction<number>>;
+  readonly setSortModel: Dispatch<SetStateAction<GridSortModel | undefined>>;
+  readonly sortModel: GridSortModel | undefined;
+  readonly setFilterModel: Dispatch<
+    SetStateAction<GridFilterModel | undefined>
+  >;
+  readonly setSelectionModel: Dispatch<
+    SetStateAction<GridSelectionModel | undefined>
+  >;
+}
+
+const DEFAULT_PAGE = 0;
+const DEFAULT_PAGE_SIZE = 10;
+
+export default function useDataGridPro({
+  defaultError,
+  defaultFilterModel,
+  defaultPage = DEFAULT_PAGE,
+  defaultPageSize = DEFAULT_PAGE_SIZE,
+  defaultSelectionModel,
+  defaultSortModel,
+  onError,
+  onFilterModelChange,
+  onPageChange,
+  onPageSizeChange,
+  onSelectModelChange,
+  onSortModelChange,
+}: Partial<Props> = DEFAULT_PROPS): State {
+  const [error, setError] = useState(defaultError);
+  const [filterModel, setFilterModel] = useState(defaultFilterModel);
+  const [page, setPage] = useState(defaultPage);
+  const [pageSize, setPageSize] = useState(defaultPageSize);
+  const [selectionModel, setSelectionModel] = useState(defaultSelectionModel);
+  const [sortModel, setSortModel] = useState(defaultSortModel);
+
+  return {
+    error,
+    filterModel,
+    handleError: useHandler(onError, setError),
+    handleFilterModelChange: useHandler(onFilterModelChange, setFilterModel),
+    handlePageChange: useHandler(onPageChange, setPage),
+    handlePageSizeChange: useHandler(onPageSizeChange, setPageSize),
+    handleSortModelChange: useHandler(onSortModelChange, setSortModel),
+    page,
+    pageSize,
+    selectionModel,
+    setError,
+    setFilterModel,
+    setPage,
+    setPageSize,
+    setSelectionModel,
+    setSortModel,
+    sortModel,
+    handleSelectionModelChange: useHandler(
+      onSelectModelChange,
+      setSelectionModel,
+    ),
+  };
 }
