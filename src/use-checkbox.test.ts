f3d2589db736c7d8398dd3bfbea08e260703068e
Charles Stover <github@charlesstover.com>
2021-10-13 02:05:22 -0500

    useCheckbox

--- src/use-checkbox.test.ts
+++ src/use-checkbox.test.ts
@@ -1,4 +1,16 @@
 import { useCheckbox } from '.';
 import describeHook from './test-utils/describe-hook';
+import mapCheckedToTestChangeEvent from './test-utils/map-checked-to-test-change-event';
 
-describeHook(useCheckbox, []);
+describeHook(useCheckbox, [
+  {
+    args: [mapCheckedToTestChangeEvent(true)],
+    callback: 'onChange',
+    defaultGetter: 'defaultChecked',
+    defaultValue: false,
+    getter: 'checked',
+    handler: 'handleChange',
+    setter: 'setChecked',
+    value: true,
+  },
+]);
--- src/use-checkbox.ts
+++ src/use-checkbox.ts
@@ -1,7 +1,36 @@
-export interface State {
-  readonly error: Error;
+import type { ChangeEvent, Dispatch, SetStateAction } from 'react';
+import { useState } from 'react';
+import DEFAULT_PROPS from './constants/default-props';
+import useHandler from './hooks/use-handler';
+
+interface Props {
+  readonly defaultChecked?: boolean | undefined;
+  readonly onChange?:
+    | ((event: ChangeEvent<HTMLInputElement>) => void)
+    | undefined;
 }
 
-export default function useCheckbox(): void {
-  return;
+export interface State {
+  readonly checked: boolean;
+  readonly handleChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  readonly setChecked: Dispatch<SetStateAction<boolean>>;
+}
+
+export default function useCheckbox({
+  defaultChecked = false,
+  onChange,
+}: Partial<Props> = DEFAULT_PROPS): State {
+  const [checked, setChecked] = useState(defaultChecked);
+
+  return {
+    checked,
+    setChecked,
+
+    handleChange: useHandler(
+      onChange,
+      (e: ChangeEvent<HTMLInputElement>): void => {
+        setChecked(e.target.checked);
+      },
+    ),
+  };
 }
