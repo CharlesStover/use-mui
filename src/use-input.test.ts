956a04416d03787f6d5667cea667235b77fa3d94
Charles Stover <github@charlesstover.com>
2021-10-13 02:43:15 -0500

    useInput

--- src/use-input.test.ts
+++ src/use-input.test.ts
@@ -1,4 +1,18 @@
 import { useInput } from '.';
 import describeHook from './test-utils/describe-hook';
+import mapValueToTestChangeEvent from './test-utils/map-value-to-test-change-event';
 
-describeHook(useInput, []);
+const TEST_VALUE = 'test value';
+
+describeHook(useInput, [
+  {
+    args: [mapValueToTestChangeEvent(TEST_VALUE)],
+    callback: 'onChange',
+    defaultGetter: 'defaultValue',
+    defaultValue: '',
+    getter: 'value',
+    handler: 'handleChange',
+    setter: 'setValue',
+    value: TEST_VALUE,
+  },
+]);
--- src/use-input.ts
+++ src/use-input.ts
@@ -1,7 +1,37 @@
-export interface State {
-  readonly error: Error;
+import type { ChangeEvent, Dispatch, SetStateAction } from 'react';
+import { useState } from 'react';
+import DEFAULT_PROPS from './constants/default-props';
+import useHandler from './hooks/use-handler';
+
+interface Props {
+  readonly defaultValue?: string | undefined;
+  readonly onChange?:
+    | ((event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void)
+    | undefined;
 }
 
-export default function useInput(): void {
-  return;
+export interface State {
+  readonly setValue: Dispatch<SetStateAction<string>>;
+  readonly value: string;
+  readonly handleChange: (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => void;
+}
+
+export default function useInput({
+  defaultValue = '',
+  onChange,
+}: Partial<Props> = DEFAULT_PROPS): State {
+  const [value, setValue] = useState(defaultValue);
+  return {
+    setValue,
+    value,
+
+    handleChange: useHandler(
+      onChange,
+      (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+        setValue(e.target.value);
+      },
+    ),
+  };
 }
