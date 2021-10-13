76adfca7437a49339e92e69bcf96484bf2c5c184
Charles Stover <github@charlesstover.com>
2021-10-13 02:38:30 -0500

    useMenu

--- src/use-menu.test.ts
+++ src/use-menu.test.ts
@@ -1,4 +1,23 @@
 import { useMenu } from '.';
 import describeHook from './test-utils/describe-hook';
 
-describeHook(useMenu, []);
+describeHook(useMenu, [
+  {
+    defaultGetter: 'defaultOpen',
+    defaultValue: false,
+    getter: 'open',
+    setter: 'setOpen',
+    value: true,
+  },
+  {
+    args: [],
+    callback: 'onClose',
+    handler: 'handleClose',
+    props: {
+      defaultOpen: true,
+    },
+    states: {
+      open: false,
+    },
+  },
+]);
