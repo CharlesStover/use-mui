828b47ecd5e893be70baae45840c41f8fa77c9fb
Charles Stover <github@charlesstover.com>
2021-10-14 02:01:34 -0500

    test DateRange and DateTime picker renders

--- src/use-date-range-picker.test.tsx
+++ src/use-date-range-picker.test.tsx
@@ -1,4 +1,8 @@
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import DateRangePicker from '@mui/lab/DateRangePicker';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import type { ReactElement } from 'react';
+import type { DateRangePickerState } from '.';
 import { useDateRangePicker } from '.';
 import DEFAULT_DATE_RANGE_PICKER_VALUE from './constants/default-date-range-picker-value';
 import describeHook from './test-utils/describe-hook';
@@ -7,41 +11,62 @@ const NOW_DATE: Date = new Date();
 const NOW_TIME: number = Date.now();
 const NOW_RANGE: [Date, number] = [NOW_DATE, NOW_TIME];
 
-describeHook(useDateRangePicker, (): ReactElement => <>{null}</>, [
-  {
-    defaultGetter: 'defaultOpen',
-    defaultValue: false,
-    getter: 'open',
-    setter: 'setOpen',
-    value: true,
-  },
-  {
-    args: [NOW_RANGE],
-    callback: 'onChange',
-    defaultGetter: 'defaultValue',
-    defaultValue: DEFAULT_DATE_RANGE_PICKER_VALUE,
-    getter: 'value',
-    handler: 'handleChange',
-    setter: 'setValue',
-    value: NOW_RANGE,
-  },
-  {
-    args: [],
-    callback: 'onClose',
-    handler: 'handleClose',
-    props: {
-      defaultOpen: true,
+describeHook(
+  useDateRangePicker,
+  ({
+    handleChange,
+    handleClose,
+    handleOpen,
+    open,
+    value,
+  }: DateRangePickerState): ReactElement => (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <DateRangePicker
+        onChange={handleChange}
+        onClose={handleClose}
+        onOpen={handleOpen}
+        open={open}
+        renderInput={(): ReactElement => <>{null}</>}
+        value={value}
+      />
+    </LocalizationProvider>
+  ),
+  [
+    {
+      defaultGetter: 'defaultOpen',
+      defaultValue: false,
+      getter: 'open',
+      setter: 'setOpen',
+      value: true,
     },
-    states: {
-      open: false,
+    {
+      args: [NOW_RANGE],
+      callback: 'onChange',
+      defaultGetter: 'defaultValue',
+      defaultValue: DEFAULT_DATE_RANGE_PICKER_VALUE,
+      getter: 'value',
+      handler: 'handleChange',
+      setter: 'setValue',
+      value: NOW_RANGE,
     },
-  },
-  {
-    args: [],
-    callback: 'onOpen',
-    handler: 'handleOpen',
-    states: {
-      open: true,
+    {
+      args: [],
+      callback: 'onClose',
+      handler: 'handleClose',
+      props: {
+        defaultOpen: true,
+      },
+      states: {
+        open: false,
+      },
     },
-  },
-]);
+    {
+      args: [],
+      callback: 'onOpen',
+      handler: 'handleOpen',
+      states: {
+        open: true,
+      },
+    },
+  ],
+);
--- src/use-date-time-picker.test.tsx
+++ src/use-date-time-picker.test.tsx
@@ -1,39 +1,64 @@
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import DateTimePicker from '@mui/lab/DateTimePicker';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import type { ReactElement } from 'react';
+import type { DateTimePickerState } from '.';
 import { useDateTimePicker } from '.';
 import describeHook from './test-utils/describe-hook';
 
 const NOW: Date = new Date();
 
-describeHook(useDateTimePicker, (): ReactElement => <>{null}</>, [
-  {
-    args: [],
-    callback: 'onOpen',
-    defaultGetter: 'defaultOpen',
-    defaultValue: false,
-    getter: 'open',
-    handler: 'handleOpen',
-    setter: 'setOpen',
-    value: true,
-  },
-  {
-    args: [NOW],
-    callback: 'onChange',
-    defaultGetter: 'defaultValue',
-    defaultValue: undefined,
-    getter: 'value',
-    handler: 'handleChange',
-    setter: 'setValue',
-    value: NOW,
-  },
-  {
-    args: [],
-    callback: 'onClose',
-    handler: 'handleClose',
-    props: {
-      defaultOpen: true,
+describeHook(
+  useDateTimePicker,
+  ({
+    handleChange,
+    handleClose,
+    handleOpen,
+    open,
+    value,
+  }: DateTimePickerState): ReactElement => (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <DateTimePicker
+        onChange={handleChange}
+        onClose={handleClose}
+        onOpen={handleOpen}
+        open={open}
+        renderInput={(): ReactElement => <>{null}</>}
+        value={value}
+      />
+    </LocalizationProvider>
+  ),
+  [
+    {
+      args: [],
+      callback: 'onOpen',
+      defaultGetter: 'defaultOpen',
+      defaultValue: false,
+      getter: 'open',
+      handler: 'handleOpen',
+      setter: 'setOpen',
+      value: true,
     },
-    states: {
-      open: false,
+    {
+      args: [NOW],
+      callback: 'onChange',
+      defaultGetter: 'defaultValue',
+      defaultValue: undefined,
+      getter: 'value',
+      handler: 'handleChange',
+      setter: 'setValue',
+      value: NOW,
     },
-  },
-]);
+    {
+      args: [],
+      callback: 'onClose',
+      handler: 'handleClose',
+      props: {
+        defaultOpen: true,
+      },
+      states: {
+        open: false,
+      },
+    },
+  ],
+);
